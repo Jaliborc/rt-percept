24d4f7a2789e0e7039f5448d661b87c31dc2d4a5
Joao Cardoso <jaliborc@arenajunkies.com>
2022-03-25 15:23:43 +0000

    Added license

--- api/loaders.py
+++ api/loaders.py
@@ -1,3 +1,12 @@
+###########################################################################
+# Copyright (c) 2022, Joao Cardoso. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+###########################################################################
+
 from torchvision.transforms.functional import to_tensor
 from torch.utils.data import Dataset
 from PIL import Image
--- train.py
+++ train.py
@@ -1,3 +1,12 @@
+###########################################################################
+# Copyright (c) 2022, Joao Cardoso. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+###########################################################################
+
 from api.loaders import *
 from api.perceptual import *
 from api.network import GeneralizedRTMetric
